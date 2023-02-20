1845fe1c9a2ab0edbbcace3beacb76aa59e87ed4
serge mirouse <sergemirouse@MBP-de-serge.lan>
2023-02-20 14:57:18 +0100

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, SafeAreaView, Image } from "react-native";
+import logo from "./assets/logo.png";
+import poster from "./assets/poster.jpg";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.body}>
+      <View>
+        <View style={styles.container}>
+          <Image source={logo} style={styles.logo} />
+        </View>
+        <View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>Interstellar</Text>
+            <View style={styles.filmInfosContainer}>
+              <Text style={styles.filmInfos}>2014 </Text>
+              <Text style={styles.filmInfos}>PG-13 </Text>
+              <Text style={styles.filmInfos}>2h49min </Text>
+              <Text style={styles.filmInfos}>Adventure, Drama, Sci-Fi</Text>
+            </View>
+            <View>
+              <View>
+                <Image source={poster} style={styles.poster} />
+              </View>
+              <View></View>
+            </View>
+          </View>
+        </View>
+        <StatusBar style={{ backgroundColor: "white" }} />
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#1D1D1D",
+    color: "white",
+  },
+  container: {
+    backgroundColor: "#393939",
+    height: 100,
+    // shadowColor: "black",
+  },
+  logo: {
+    maxWidth: 100,
+    resizeMode: "contain",
+    marginTop: -30,
+    marginLeft: 20,
+  },
+  titleContainer: {
+    marginTop: 20,
+    marginLeft: 20,
+  },
+  title: {
+    fontSize: 40,
+    color: "white",
+  },
+  filmInfosContainer: {
+    flexDirection: "row",
+    // justifyContent: "space-around",
+    marginTop: 15,
+  },
+  filmInfos: {
+    color: "#B0B0B0",
+  },
+  poster: {
+    width: 200,
+    resizeMode: "contain",
+    top: -450,
   },
 });
